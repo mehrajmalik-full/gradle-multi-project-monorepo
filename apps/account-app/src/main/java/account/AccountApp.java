5f55c571117af72773ea5aa001809cbde71ab2aa
mehraj malik <mehrajuddin.malik@anywhere.co>
2026-02-03 12:35:38 +0530

    AccountApp updated with cache

--- apps/account-app/src/main/java/account/AccountApp.java
+++ apps/account-app/src/main/java/account/AccountApp.java
@@ -19,9 +19,5 @@ public class AccountApp {
     public int sum(int a, int b) {
         return a + b;
     }
-
-    public int multiply(int a, int b) {
-        return a * b;  // NEW METHOD ADDED
-    }
 }
 
